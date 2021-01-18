6526cc308f460196b4d31a89c50a52ed34cb89eb
abdurashiidov <abdurashiidov18@gmail.com>
2021-01-18 09:51:53 +0300

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const input = document.querySelector(".input")
+let Title = document.querySelector(".title")
+
+
+
+input.addEventListener('keyup', () => {
+    Title.textContent = input.value
+
+    if(Title.textContent === 'hack'){
+        document.body.style.backgroundColor = 'red'
+    } else if (Title.textContent === 'done'){
+        document.body.style.backgroundColor = 'lime'
+    } else {
+        document.body.style.backgroundColor = 'grey'
+    }
+})
+
+
+
+
+
